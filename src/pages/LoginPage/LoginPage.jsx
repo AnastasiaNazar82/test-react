0bcf228b8a7e9140d2f1acf4fe57aae0b91a5854
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-09-02 17:21:14 +0300

    add

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,10 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
+import style from "./LoginPage.module.css";
 
 export default function LoginPage() {
   return (
-    <>
+    <div className={style.background}>
       <LoginForm />
-    </>
+    </div>
   );
 }
