e6574b93841bd3f7fbe588d41c270d24c85190cd
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-09-02 13:37:35 +0300

    add

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,9 @@
 import LoginForm from "../../components/LoginForm/LoginForm";
-import AuthMainPage from "../../components/AuthMain/AuthMain";
 
 export default function LoginPage() {
   return (
     <>
-      <title>Sign in page</title>
-
-      <AuthMainPage>
-        <LoginForm />
-      </AuthMainPage>
+      <LoginForm />
     </>
   );
 }
