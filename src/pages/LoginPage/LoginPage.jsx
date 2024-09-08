e43f5cb4eee6d48580d01dadc73b4c00dde5a9b2
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-09-08 11:53:12 +0300

    add

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,26 @@
-import LoginForm from "../../components/LoginForm/LoginForm";
-import style from "./LoginPage.module.css";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import LoginForm from 'components/LoginForm/LoginForm.jsx';
+import { selectIsLoading } from '..//../redux/auth/auth.selectors';
+import Loader from 'components/Loader/Loader.jsx';
+import style from './SignInPage.module.css';
 
-export default function LoginPage() {
+const SignInPage = () => {
+  const isLoader = useSelector(selectIsLoading);
+
   return (
-    <div className={style.background}>
-      <LoginForm />
+    <div className={style.container}>
+      <div className={style.background}>
+        {isLoader ? (
+          <Loader />
+        ) : (
+          <>
+            <LoginForm />
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default SignInPage;
